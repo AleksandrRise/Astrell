f8142000886b0ca79ba2faf160e5909be1efe74f
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-19 17:17:34 -0700

    Added AI model to Python but it acts weirdly

--- python-service/app.py
+++ python-service/app.py
@@ -1,12 +1,33 @@
+import moviepy.editor as mp
+import speech_recognition as sr
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Hello World!"
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
+
+# Load the video
+video = mp.VideoFileClip("top5box.mp4")
+
+# Extract the audio from the video
+audio_file = video.audio
+audio_file.write_audiofile("top5box.wav")
+
+# Initialize recognizer
+r = sr.Recognizer()
+
+# Load the audio file
+with sr.AudioFile("top5box.wav") as source:
+    data = r.record(source)
+
+# Convert speech to text
+text = r.recognize_google(data)
+
+# Print the text
+print("\nThe resultant text from video is: \n")
+print(text)
