4bff4268798ae14b218cbe5db5abe9bbdcc106fc
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-19 18:15:07 -0700

    Added an AI video-to-text model using Whisper

--- python-service/app.py
+++ python-service/app.py
@@ -1,33 +1,13 @@
-import moviepy.editor as mp
-import speech_recognition as sr
+import whisper
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello World!"
+    return "hello world!"
 
-if __name__ == "__main__":
-    app.run()
 
-# Load the video
-video = mp.VideoFileClip("top5box.mp4")
-
-# Extract the audio from the video
-audio_file = video.audio
-audio_file.write_audiofile("top5box.wav")
-
-# Initialize recognizer
-r = sr.Recognizer()
-
-# Load the audio file
-with sr.AudioFile("top5box.wav") as source:
-    data = r.record(source)
-
-# Convert speech to text
-text = r.recognize_google(data)
-
-# Print the text
-print("\nThe resultant text from video is: \n")
-print(text)
+model = whisper.load_model("base")
+result = model.transcribe("test.mp4")
+print(result["text"])
