1886f4b6ec398e458c758e471adbca7a74e22df5
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-20 17:47:58 -0700

    Managed to parse an AI response into JSON in a certain format

--- python-service/app.py
+++ python-service/app.py
@@ -1,6 +1,7 @@
 import whisper
 from flask import Flask
 from meta_ai_api import MetaAI
+import json
 
 app = Flask(__name__)
 
@@ -13,8 +14,12 @@ ai = MetaAI()
 model = whisper.load_model("base")
 result = model.transcribe("test.mp4")
 
-msg = "Explain here what this video is about in a few words. Transcript: " + result["text"]
+msg = ('Make a single quiz according to a transcript and following the format without adding any your commects at all: {"question": "(your question)", "answer1": "(your answer 1)", "answer2": "(your answer 2)", '
+       '"answer3": "(your answer 3)", "answer4": "(your answer 4)"} . Transcript you use: ') + result["text"]
 response = ai.prompt(message=msg)
 
-print(response['message'])
+res = json.loads(response['message'])
+print(res["question"])
+
+
 
