c97279b5b44ff926e70799ce181b4790d6ef2be0
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-20 18:21:52 -0700

    Added more prompts and sorted commands in functions

--- python-service/app.py
+++ python-service/app.py
@@ -14,12 +14,35 @@ ai = MetaAI()
 model = whisper.load_model("base")
 result = model.transcribe("test.mp4")
 
-msg = ('Make a single quiz according to a transcript and following the format without adding any your commects at all: {"question": "(your question)", "answer1": "(your answer 1)", "answer2": "(your answer 2)", '
-       '"answer3": "(your answer 3)", "answer4": "(your answer 4)"} . Transcript you use: ') + result["text"]
-response = ai.prompt(message=msg)
+def getQuizJSON():
+    msg = ('Make a single quiz according to a transcript and following the format without adding any your comments at all: '
+           '{"question": "(your question)", "answer1": "(your answer 1)", "answer2": "(your answer 2)", '
+            '"answer3": "(your answer 3)", "answer4": "(your answer 4)"} . Transcript you use: ') + result["text"]
+    convertToJSON(msg)
 
-res = json.loads(response['message'])
-print(res["question"])
+def getCardJSON():
+    msg = ('Make a single question and answer according to a transcript and following the format without adding any of your comments at all: '
+           '{"question": "(the question)", "answer": "(the answer to the question)" . Transcript you use: ') + result["text"]
+    convertToJSON(msg)
+
+def getHighlightStr() -> str:
+    msg = ('Highlight key concepts according to a transcript. The text must be comprehensive enough for a student at a college. Transcript you use: ') + result["text"]
+    return convertToStr(msg)
+
+def getSummarizeStr() -> str:
+    msg = ('Make a summarization of a transcript in 200-300 words without adding any of your comments at all. The text must be comprehensive enough for a student at a college. Transcript you use: ') + result["text"]
+    return convertToStr(msg)
+
+def convertToJSON(msg: str):
+    response = ai.prompt(message=msg)
+    return json.loads(response['message'])
+
+def convertToStr(msg: str) -> str:
+    response = ai.prompt(message=msg)
+    return response['message']
+
+
+print(getHighlightStr())
 
 
 
