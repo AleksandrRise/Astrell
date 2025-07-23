a140aa03123bd52d7681f9451e811ddffb6840bd
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-23 10:43:08 -0700

    Added a new ResponsePage to the router and added a background

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage"; 
+import ResponsePage from "./pages/ResponsePage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/response" element={<ResponsePage />} />
     </Routes>
   )
 }
--- client/src/pages/ResponsePage.tsx
+++ client/src/pages/ResponsePage.tsx
@@ -4,8 +4,10 @@ import Navbar from "../shared/components/Navbar";
 
 
 export default function ResponsePage() {
+    const mainClasses = "h-screen w-full hero-bg"
+
     return (
-        <main>
+        <main className={mainClasses}>
             <Navbar>
                 <Navbar.MenuBtn />
             </Navbar>
