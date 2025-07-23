d09d6880e1d588853582f4386f0cf0bff4bc3cb8
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-22 19:01:40 -0700

    Added an animation to the aiball

--- client/src/features/home/components/AiBall.tsx
+++ client/src/features/home/components/AiBall.tsx
@@ -7,12 +7,13 @@ type AiBallType = {
 
 export default function AiBall({ children }: AiBallType) {
     const imgAlt = "AI Thinking Ball"
+    const figClasses = "zoomIn"
     const imgClasses = "aiball-animation"
     const sectionClasses = "m-auto"
 
     return (
         <section className={sectionClasses}>
-            <figure>
+            <figure className={figClasses}>
                 <img className={imgClasses} width="450px" height="450px" src={imgSrc} alt={imgAlt} />
             </figure>
 
