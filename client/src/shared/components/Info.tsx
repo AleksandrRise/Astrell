feebccffab8652a46dfa3e7bd0da84995f6581f2
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-23 10:19:05 -0700

    Added light to the hint button

--- client/src/shared/components/Info.tsx
+++ client/src/shared/components/Info.tsx
@@ -23,8 +23,8 @@ export default function Info({ children }: InfoProps) {
     }, [])
 
     const infoAlt = "Info"
-    const wrapperClasses = "rounded-full bg-white/30 w-17.5 h-17.5 flex cursor-pointer \
-    hover:bg-white/50 transition fadeIn opacity-0 relative"
+    const wrapperClasses = `rounded-full ${!isOpen && "hover:bg-white/50"} bg-white/30 w-17.5 h-17.5 flex cursor-pointer \
+    transition fadeIn opacity-0 relative ${isOpen && "hint-open"}`
     const imgClasses = "w-10 h-10 m-auto"
 
     return (
