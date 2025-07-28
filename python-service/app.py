600e086c7df7fd64beb23a5f6949ecb5f4905ad2
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-28 15:31:55 -0700

    Commit to keep python up-to-date

--- python-service/app.py
+++ python-service/app.py
@@ -1,5 +1,5 @@
 import whisper
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_cors import CORS
 from meta_ai_api import MetaAI
 
@@ -7,26 +7,33 @@ from commands import Commands
 
 app = Flask(__name__)
 origin = "http://localhost:5173"
-CORS(app)
+CORS(app, resources={r"*": {"origins": origin}})
 
 ai = MetaAI()
 model = whisper.load_model("base")
 
+transcript = ""
+video = ""
+
 
 @app.route('/api/v1/uploadVideo', methods=['POST'])
-def uploadVideo():
+def uploadVideo() -> str:
+    global video
     video = request.files['file']
-    filename = video.filename
+    video.save(f"videos/temp")
 
-    video.save(f"videos/{filename}")
-
-    transcript = model.transcribe(f"videos/{filename}")
-    print(transcript["text"])
+    global transcript
+    transcript = model.transcribe(f"videos/temp")
 
     return transcript
 
 
+@app.route('/api/v1/summarize', methods=['GET'])
+def summarize() -> str:
+    print(transcript)
+    commands = Commands(transcript, ai)
+    return commands.getSummarizeStr()
+
+
 if __name__ == "__main__":
     app.run(port=5000)
-
-# commands = Commands(transcript, ai)
