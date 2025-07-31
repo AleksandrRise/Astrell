4add7268522fa6560239c11f259b316e39d6c7b6
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-30 20:30:10 -0700

    Made a profile block on the bottom of Aside

--- client/src/features/test/components/Profile.tsx
+++ client/src/features/test/components/Profile.tsx
@@ -1,5 +1,20 @@
+import profileIcon from "../assets/ProfileIcon.png"
+
 export default function Profile() {
+
+    // Classes
+    const btnClasses = "mt-10 w-full flex items-center gap-3 \
+        shadow-[0_1px_0_0_#333533_inset] rounded-3xl bg-linear-to-r \
+        from-white/5 via-[#999999]/5 to-white/10 px-5 py-3.5 mb-11.25"
+    const imgClasses = "w-11.25 h-11.25"
+    const spanClasses = "text-base text-white/80"
+
+    const altIcon = "Guest Icon"
+
     return (
-        <h1>Profile</h1>
+        <button className={btnClasses}>
+            <img className={imgClasses} src={profileIcon} alt={altIcon} />
+            <span className={spanClasses}>Guest</span>
+        </button>
     )
 }
