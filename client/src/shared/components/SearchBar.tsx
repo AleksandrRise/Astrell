0e6c57fcf9cac3f12fa157f231ffb3917f9bdee9
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-30 18:29:45 -0700

    Made a search bar along with styling

--- client/src/shared/components/SearchBar.tsx
+++ client/src/shared/components/SearchBar.tsx
@@ -1,5 +1,21 @@
+import searchIcon from "../../assets/Search.png"
+
 export default function SearchBar() {
+    // Classes
+    const altSearch = "Search"
+    const imgClasses = "w-5 h-5"
+    const inputPlaceholder = "Search..."
+    const inputClasses = "outline-none border-none ml-3.5 text-sm \
+        placeholder:text-white/40 font-light"
+    const wrapperClasses = "w-full h-11.25 px-3.5 flex items-center mt-5.5 \
+        shadow-[0_0_0_1px_#333533] stroke-[#333533] bg-white/5 rounded-xl \
+        cursor-text"
+    const htmlFor = "searchbar"
+
     return (
-        <h1>Search bar</h1>
+        <label htmlFor={htmlFor} className={wrapperClasses}>
+            <img className={imgClasses} src={searchIcon} alt={altSearch} />
+            <input className={inputClasses} type="text" name={htmlFor} id={htmlFor} placeholder={inputPlaceholder} />
+        </label>
     )
 }
