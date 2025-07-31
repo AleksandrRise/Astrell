2e4a0e6555c6caa4943b3eac58d9497e63547a35
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-30 20:45:51 -0700

    Added animation to hover for feature components

--- client/src/features/test/components/Feature.tsx
+++ client/src/features/test/components/Feature.tsx
@@ -12,12 +12,14 @@ export default function Feature({ name, icon, isActive }: FeatureProps) {
         ${isActive 
             ? "text-white/90 shadow-[0_0_0_1px_#333533] \
                 bg-linear-to-r from-[#232323]/50 to-white/15 to-60%" 
-            : "text-white/50"
+            : "text-white/50 hover:shadow-[0_0_0_1px_#333533]/50 \
+                hover:bg-linear-to-r hover:from-[#232323]/35 \
+                hover:to-white/10 hover:to-60%"
         }`
     const btnClasses = `flex items-center gap-3.75 px-6 py-2.5 w-full rounded-lg \
         ${isActive
             ? "shadow-[0_1px_0_0_#333533_inset]"
-            : "cursor-pointer"
+            : "hover:shadow-[0_1px_0_0_#333533_inset]/50 cursor-pointer"
         }`
     const imgClasses = `w-7.5 h-7.5 ${isActive ? "opacity-90" : "opacity-50"}`
     const spanClasses = ""
