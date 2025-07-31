2a7f4ee1edf7c3bdaf47d8239e33cdaae701c44c
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-30 17:48:58 -0700

    Made proper sizing for the LogoSection component

--- client/src/features/test/components/LogoSection.tsx
+++ client/src/features/test/components/LogoSection.tsx
@@ -2,7 +2,7 @@ import Logo from "../../../shared/components/Logo"
 
 export default function LogoSection() {
     // Classes
-    const wrapperClasses = "flex items-center text-3xl cursor-default py-7.5"
+    const wrapperClasses = "flex items-center text-3xl cursor-default pt-10 pb-2"
     const h1Classes = "pt-1 tracking-wider font-bold font-lato"
 
     return (
