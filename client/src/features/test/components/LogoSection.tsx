b0c8c6d42fd28adfba4e03e66055e40d325fcc22
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-30 17:46:11 -0700

    Made a cursor-default for the LogoSection component

--- client/src/features/test/components/LogoSection.tsx
+++ client/src/features/test/components/LogoSection.tsx
@@ -2,8 +2,7 @@ import Logo from "../../../shared/components/Logo"
 
 export default function LogoSection() {
     // Classes
-    const wrapperClasses = "flex items-center text-3xl \
-    py-7.5"
+    const wrapperClasses = "flex items-center text-3xl cursor-default py-7.5"
     const h1Classes = "pt-1 tracking-wider font-bold font-lato"
 
     return (
