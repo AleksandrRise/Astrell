1c9a946883bd0c0dd6fc64900e8aee4bb2f58adc
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-07-30 19:58:55 -0700

    Made a features section

--- client/src/features/test/components/Aside.tsx
+++ client/src/features/test/components/Aside.tsx
@@ -3,9 +3,21 @@ import LogoSection from "./LogoSection";
 import SearchBar from "../../../shared/components/SearchBar";
 import Other from "./Other"
 import Profile from "./Profile"
+import dashboardIcon from "../assets/DashboardIcon.png"
+import chatIcon from "../assets/ChatIcon.png"
+import examIcon from "../assets/ExamIcon.png"
+import historyIcon from "../assets/HistoryIcon.png"
 
 
 export default function Aside() {
+
+    const features = [
+        { name: "Dashboard", icon: dashboardIcon, isActive: true},
+        { name: "History", icon: historyIcon, isActive: false},
+        { name: "Exam Builder", icon: examIcon, isActive: false},
+        { name: "Chat", icon: chatIcon, isActive: false},
+    ]
+
     // Classes
     const asideClasses = "text-white w-85 h-screen bg-blackBG border-r-3 \
     border-white/5 px-8.5"
@@ -20,13 +32,19 @@ export default function Aside() {
             <SearchBar />
 
             <Features>
-                <Features.Feature />
+                {features.map(feature => 
+                    <Features.Feature 
+                        name={feature.name} 
+                        icon={feature.icon} 
+                        isActive={feature.isActive}
+                    />
+                )}
             </Features>
 
             <div className={divClasses}></div>
 
             <Other>
-                <Other.Feature />
+                <Other.Feature name="Settings" icon="" isActive={false} />
             </Other>
             
             <Profile />
--- client/src/features/test/components/Feature.tsx
+++ client/src/features/test/components/Feature.tsx
@@ -1,5 +1,36 @@
-export default function Feature() {
+
+type FeatureProps = {
+    name: string;
+    icon: string;
+    isActive: boolean;
+}
+
+export default function Feature({ name, icon, isActive }: FeatureProps) {
+
+    // Classes
+    const liClasses = `w-full rounded-lg  \
+        ${isActive 
+            ? "text-white/90 shadow-[0_0_0_1px_#333533] \
+                bg-linear-to-r from-[#232323]/50 to-white/15 to-60%" 
+            : "text-white/50"
+        }`
+    const btnClasses = `flex items-center gap-3.75 px-6 py-2.5 w-full rounded-lg \
+        ${isActive
+            ? "shadow-[0_1px_0_0_#333533_inset]"
+            : "cursor-pointer"
+        }`
+    const imgClasses = `w-7.5 h-7.5 ${isActive ? "opacity-90" : "opacity-50"}`
+    const spanClasses = ""
+
+    // Properties
+    const altIcon = "Icon"
+
     return (
-        <h1>feature</h1>
+        <li className={liClasses}>
+            <button className={btnClasses}>
+                <img className={imgClasses} src={icon} alt={altIcon} />
+                <span className={spanClasses}>{name}</span>
+            </button>
+        </li> 
     )
 }
