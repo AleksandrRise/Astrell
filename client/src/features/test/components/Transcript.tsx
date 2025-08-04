aafb6b00218d41a9e6ea231aa1db926f9d2f0e2a
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-04 11:39:45 -0700

    Added a border to the transcript block

--- client/src/features/test/components/Transcript.tsx
+++ client/src/features/test/components/Transcript.tsx
@@ -1,42 +1,47 @@
 export default function Transcript() {
 
     // Classes
-    const wrapperClasses = "outline-solid rounded-3xl w-full h-full row-start-2"
+    const wrapperClasses = "relative rounded-3xl w-full h-full row-start-2 p-[1px] \
+        bg-gradient-to-tl from-[#333333] from-[82%] to-[#999999] to-[100%] z-1"
+    const innerWrapperClasses = "bg-blackBG rounded-3xl w-full h-full"
 
     return (
         <article className={wrapperClasses}>
-            <button></button>
+            <div className={innerWrapperClasses}>
+                <button></button>
 
-            <div>
-                <button>
-                    <img src="" alt="" />
-                    <span>Transcription</span>
-                </button>
-                <button>
-                    <img src="" alt="" />
-                    <span>Summary</span>
-                </button>
-                <button>
-                    <img src="" alt="" />
-                    <span>Highlights</span>
-                </button>
+                <div>
+                    <button>
+                        <img src="" alt="" />
+                        <span>Transcription</span>
+                    </button>
+                    <button>
+                        <img src="" alt="" />
+                        <span>Summary</span>
+                    </button>
+                    <button>
+                        <img src="" alt="" />
+                        <span>Highlights</span>
+                    </button>
+                </div>
+
+                <div></div>
+
+                <div>
+                    <p>
+                        Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so 
+                        what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so 
+                        what yo bro. 
+
+                        Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so 
+                        what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what ...hen what so 
+
+                        what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what ... hen what so 
+                        what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so                        
+                    </p>
+                </div>
             </div>
-
-            <div></div>
-
-            <div>
-                <p>
-                    Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so 
-                    what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so 
-                    what yo bro. 
-
-                    Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so 
-                    what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what ...hen what so 
-
-                    what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what ... hen what so 
-                    what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so                        
-                </p>
-            </div>
+            
         </article>
     )
 }
