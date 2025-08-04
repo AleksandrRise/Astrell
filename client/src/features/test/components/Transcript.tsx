24f2dfd733b632f9b4d8291a05eeae4fc76ce695
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-04 12:18:47 -0700

    Added overflow-hidden to the main class of the response page and made some changes to the transcript block

--- client/src/features/test/components/Transcript.tsx
+++ client/src/features/test/components/Transcript.tsx
@@ -1,43 +1,61 @@
+import transcriptIcon from "../assets/TranscriptIcon.png"
+import summaryIcon from "../assets/SummaryIcon.png"
+import highlightsIcon from "../assets/HighlightsIcon.png"
+
+
+type buttonType = {
+    icon: string;
+    alt: string;
+    name: string;
+    isActive: boolean;
+}
+
 export default function Transcript() {
+
+    const buttons: buttonType[] = [
+        {icon: transcriptIcon, alt: "transcriptIcon", name: "Transcription", isActive: true},
+        {icon: summaryIcon, alt: "summaryIcon", name: "Summary", isActive: false},
+        {icon: highlightsIcon, alt: "highlightsIcon", name: "Highlights", isActive: false},
+    ]
 
     // Classes
     const wrapperClasses = "relative rounded-3xl w-full h-full row-start-2 p-[1px] \
         bg-gradient-to-tl from-[#333333] from-[82%] to-[#999999] to-[100%] z-1"
-    const innerWrapperClasses = "bg-blackBG rounded-3xl w-full h-full"
+    const innerWrapperClasses = "bg-blackBG rounded-3xl w-full h-full px-13.5"
+    const btnsClasses = "flex gap-11"
+    const btnClasses = "flex gap-2.5 justify-center"
+    const iconClasses = "w-7.5 h-7.5"
+    const btnTextClasses = "text-xl font-bold font-lato"
+    const lineClasses = "my-4 w-full h-[1px] bg-white/5"
+    const textWrapperClasses = "w-full h-full"
+    const textClasses = "overflow-auto"
 
     return (
         <article className={wrapperClasses}>
             <div className={innerWrapperClasses}>
                 <button></button>
 
-                <div>
-                    <button>
-                        <img src="" alt="" />
-                        <span>Transcription</span>
-                    </button>
-                    <button>
-                        <img src="" alt="" />
-                        <span>Summary</span>
-                    </button>
-                    <button>
-                        <img src="" alt="" />
-                        <span>Highlights</span>
-                    </button>
+                <div className={btnsClasses}>
+                    {buttons.map(btn => {
+                        return (
+                            <button className={btnClasses}>
+                                <img className={iconClasses} src={btn.icon} alt={btn.alt} />
+                                <span className={btnTextClasses}>{btn.name}</span>
+                            </button>
+                        )
+                    })}
                 </div>
 
-                <div></div>
+                <div className={lineClasses}></div>
 
-                <div>
-                    <p>
+                <div className={textWrapperClasses}>
+                    <p className={textClasses}>
                         Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so 
                         what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so 
                         what yo bro. 
 
                         Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so 
-                        what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what ...hen what so 
-
-                        what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what ... hen what so 
-                        what yo bro. Some text bla yes yes and then what so what yo bro. Some text bla yes yes and then what so                        
+                        what ifaepf and then what so what yo bro. Some text bla yes yes and then what so                        
                     </p>
                 </div>
             </div>
