b41ebe1b45d4c6fe93fa768f992caab26feb5f7d
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-05 16:55:21 -0700

    Centered text for the Notes block

--- client/src/features/test/components/Notes.tsx
+++ client/src/features/test/components/Notes.tsx
@@ -1,12 +1,15 @@
 export default function Notes() {
 
     // Classes
-    const wrapperClasses = "outline-solid w-full h-auto row-start-1 rounded-3xl"
+    const wrapperClasses = "outline-solid w-full h-auto row-start-1 rounded-3xl \
+        relative flex flex-col items-center"
+    const headerClasses = "font-lato text-2xl font-bold mt-8"
+    const textClasses = "my-auto text-5xl font-lato font-bold"
 
     return (
         <section className={wrapperClasses}>
-            <h2>My Notes</h2>
-            <span>N/A</span>
+            <h2 className={headerClasses}>My Notes</h2>
+            <span className={textClasses}>N/A</span>
         </section>
     )
 }
