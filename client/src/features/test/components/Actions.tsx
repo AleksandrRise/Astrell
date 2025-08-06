a763b2908f5e8cc211708dbf3df7aa3ad37c2fee
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-05 20:16:09 -0700

    Declared all the classes as variables including alt for img

--- client/src/features/test/components/Actions.tsx
+++ client/src/features/test/components/Actions.tsx
@@ -11,28 +11,42 @@ export default function Actions() {
     const wrapperClasses = "h-full w-full grid grid-cols-2 grid-rows-2 gap-5.25 \
         row-start-2"
     const btnClasses = "rounded-3xl logoGradient-bg p-[2px]"
-    const labelClasses = "logoGradient-bg p-[2px] w-full rounded-3xl col-span-2"
+    const labelClasses = "logoGradient-bg p-[5px] w-full rounded-3xl col-span-2"
+    const quizSubtextClasses = ""
+    const quizIconClasses = ""
+    const quizIconAlt = "QuizIcon"
+    const quizTextClasses = ""
+    const flashIconClasses = ""
+    const flashIconAlt = "FlashcardsIcon"
+    const flashTextClasses = ""
+    const flashSubtextClasses = ""
+    const aiSubtextClasses = ""
+    const aiTextClasses = ""
+    const aiInputWrapperClasses = ""
+    const aiInputClasses = ""
+    const aiIconClasses = ""
+    const aiIconAlt = "AIIcon"
 
     return (
         <section className={wrapperClasses}>
             <button className={btnClasses}>
-                <span>Make a</span>
-                <img src={QuizIcon} alt="" />
-                <span>Quiz</span>
+                <span className={quizSubtextClasses}>Make a</span>
+                <img className={quizIconClasses} src={QuizIcon} alt={quizIconAlt} />
+                <span className={quizTextClasses}>Quiz</span>
             </button>
 
             <button className={btnClasses}>
-                <img src={FlashIcon} alt="" />
-                <span>Flashcards</span>
-                <span>Make</span>
+                <img className={flashIconClasses} src={FlashIcon} alt={flashIconAlt} />
+                <span className={flashTextClasses}>Flashcards</span>
+                <span className={flashSubtextClasses}>Make</span>
             </button>
 
             <label className={labelClasses} htmlFor={htmlFor}>
-                <span>Ask</span>
-                <span>AI</span>
-                <div>
-                    <input type="text" name={htmlFor} id={htmlFor} />
-                    <img src={AIIcon} alt="" />
+                <span className={aiSubtextClasses}>Ask</span>
+                <span className={aiTextClasses}>AI</span>
+                <div className={aiInputWrapperClasses}>
+                    <input className={aiInputClasses} type="text" name={htmlFor} id={htmlFor} />
+                    <img className={aiIconClasses} src={AIIcon} alt={aiIconAlt} />
                 </div>
             </label>
         </section>
