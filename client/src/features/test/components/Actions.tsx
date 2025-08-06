26272a45afd0839866d42027dfd174c92cba9353
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-05 21:06:19 -0700

    Finished styling the last actions button. Still needs to fix some things

--- client/src/features/test/components/Actions.tsx
+++ client/src/features/test/components/Actions.tsx
@@ -8,11 +8,10 @@ export default function Actions() {
     const htmlFor = "askai"
 
     // Classes
-    const wrapperClasses = "h-full w-full grid grid-cols-2 grid-rows-2 gap-5.25 \
+    const wrapperClasses = "size-full grid grid-cols-2 grid-rows-2 gap-5.25 \
         row-start-2 font-lato"
     const btnClasses = "rounded-3xl logoGradient-bg p-[2px] flex flex-col text-start \
         justify-center items-center cursor-pointer"
-    const labelClasses = "logoGradient-bg p-[5px] w-full rounded-3xl col-span-2"
     const quizContainerClasses = ""
     const quizTextClasses = "text-4xl font-bold bg-clip-text text-transparent \
         bg-linear-to-l from-[#999999] to-white"
@@ -26,12 +25,17 @@ export default function Actions() {
     const flashTextClasses = "text-2xl font-bold bg-clip-text text-transparent \
         bg-linear-to-l from-[#999999] to-white"
     const flashSubtextClasses = "text-sm italic ml-auto font-semibold"
-    const aiContainerClasses = ""
-    const aiSubtextClasses = ""
-    const aiTextClasses = ""
-    const aiInputWrapperClasses = ""
-    const aiInputClasses = ""
-    const aiIconClasses = "size-7"
+    const aiClasses = "logoGradient-bg p-[5px] rounded-3xl col-span-2 \
+        flex justify-center items-center cursor-text"
+    const aiContainerClasses = "flex items-end gap-5.5"
+    const aiTextClasses = "text-4xl bg-clip-text text-transparent \
+        bg-linear-to-l from-[#999999] to-white"
+    const aiSubtextClasses = "text-sm italic"
+    const aiTextWrapperClasses = "relative flex flex-col"
+    const aiInputWrapperClasses = "relative w-70"
+    const aiInputClasses = "border border-[#333533] rounded-lg bg-white/5 outline-none \
+        p-2 w-full"
+    const aiIconClasses = "size-7 absolute bottom-1.5 right-2.5 cursor-pointer"
     const aiIconAlt = "AiIcon"
 
     return (
@@ -54,13 +58,15 @@ export default function Actions() {
                 </div>
             </button>
 
-            <label className={labelClasses} htmlFor={htmlFor}>
+            <label className={aiClasses} htmlFor={htmlFor}>
                 <div className={aiContainerClasses}>
-                    <span className={aiSubtextClasses}>Ask</span>
-                    <span className={aiTextClasses}>AI</span>
+                    <div className={aiTextWrapperClasses}>
+                        <span className={aiSubtextClasses}>Ask</span>
+                        <span className={aiTextClasses}>AI</span>
+                    </div>
                     <div className={aiInputWrapperClasses}>
-                        <input className={aiInputClasses} type="text" name={htmlFor} id={htmlFor} />
                         <img className={aiIconClasses} src={AIIcon} alt={aiIconAlt} />
+                        <input className={aiInputClasses} type="text" name={htmlFor} id={htmlFor} />
                     </div>
                 </div>
             </label>
