1188a670897984a2c0ef073e0f35b84640d1fe8c
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-06 20:13:14 -0700

    Added a flashcard request

--- client/src/features/test/components/Action2.tsx
+++ client/src/features/test/components/Action2.tsx
@@ -1,10 +1,28 @@
 import FlashIcon from "../assets/FlashIcon.png"
+import axios from "axios"
+import { useState } from "react"
 
 export default function Action2() {
 
+    const [ isLoading, setIsLoading ] = useState<boolean>(false)
+
+    const ADDRESS: string = "http://127.0.0.1:5000"
+
+    const fetchData = async () => {
+        setIsLoading(true)
+        const response = await axios.get(`${ADDRESS}/api/v1/getcard`)
+
+        if (response.status != 200) {
+            console.error(response.statusText)
+        } else {
+            console.log(response.data)
+        }
+        setIsLoading(false)
+    }
+
     // Classes
-    const btnClasses = "rounded-3xl logoGradient-bg p-[2px] flex flex-col text-start \
-        justify-center items-center cursor-pointer"
+    const btnClasses = `rounded-3xl logoGradient-bg p-[2px] flex flex-col text-start \
+        justify-center items-center cursor-pointer ${isLoading && "animate-pulse"}`
     const flashContainerClasses = "flex flex-col"
     const flashIconClasses = "size-7"
     const flashIconAlt = "FlashcardsIcon"
@@ -13,7 +31,7 @@ export default function Action2() {
     const flashSubtextClasses = "text-sm italic ml-auto font-semibold"
 
     return (
-        <button className={btnClasses}>
+        <button className={btnClasses} onClick={() => fetchData()}>
             <div className={flashContainerClasses}>
                 <img className={flashIconClasses} src={FlashIcon} alt={flashIconAlt} />
                 <span className={flashTextClasses}>Flashcards</span>
