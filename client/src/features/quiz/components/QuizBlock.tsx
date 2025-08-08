027bc9be761a707dd77e4cf99a4fb99468ea84d1
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-08 15:20:24 -0700

    Added a title for quiz settings

--- client/src/features/quiz/components/QuizBlock.tsx
+++ client/src/features/quiz/components/QuizBlock.tsx
@@ -1,21 +1,32 @@
 import DifficultyBlock from "./DifficultyBlock"
 import QuestionsNumBlock from "./QuestionsNumBlock"
 import TimeBlock from "./TimeBlock"
+import arrowIcon from "../assets/arrow.png"
 
 export default function QuizBlock() {
+
+    // Attributes
+    const arrowIconAlt = "GoBack"
     
     // Classes
-    const wrapperClasses = ""
-    const containerClasses = ""
-    const titleClasses = ""
+    const wrapperClasses = "text-white rounded-[40px] bg-white/8 size-fit \
+        backdrop-blur-[100px] shadow-[0_0_2px_0_rgba(0,149,255,75)_inset]"
+    const containerClasses = "p-11.5"
+    const titleClasses = "flex gap-11.5 items-center"
+    const titleTextClasses = "font-lato font-bold text-2xl"
+    const btnBackClasses = "bg-white/20 rounded-full size-10 cursor-pointer \
+        flex justify-center items-center"
+    const iconBackClasses = "size-4.5"
     const descClasses = ""
 
     return (
         <section className={wrapperClasses}>
             <div className={containerClasses}>
                 <div className={titleClasses}>
-                    <button></button>
-                    <h1>Quiz Settings</h1>
+                    <button className={btnBackClasses}>
+                        <img className={iconBackClasses} src={arrowIcon} alt={arrowIconAlt} />
+                    </button>
+                    <h1 className={titleTextClasses}>Quiz Settings</h1>
                 </div>
 
                 <span className={descClasses}>Choose your quiz preferences.</span>
