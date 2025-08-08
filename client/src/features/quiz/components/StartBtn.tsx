4b4d147c8477b67d65395bddca1932d728b3b529
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-08 15:54:06 -0700

    Added a Start button

--- client/src/features/quiz/components/StartBtn.tsx
+++ client/src/features/quiz/components/StartBtn.tsx
@@ -1,9 +1,10 @@
 export default function StartBtn() {
 
     // Classes
-    const btnClasses = ""
+    const btnClasses = "mt-9.25 w-full py-3 bg-white/20 font-lato text-base \
+        rounded-xl font-bold tracking-wider cursor-pointer"
 
     return (
-        <button className={btnClasses}>Start</button>
+        <button className={btnClasses}>Start!</button>
     )
 }
