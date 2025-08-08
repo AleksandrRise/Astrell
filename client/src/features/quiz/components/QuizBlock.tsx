16b55402386391085c2d2d5c9e9336b01a57ec5e
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-08 15:50:36 -0700

    Added tracking-wide for the description text

--- client/src/features/quiz/components/QuizBlock.tsx
+++ client/src/features/quiz/components/QuizBlock.tsx
@@ -9,7 +9,7 @@ export default function QuizBlock() {
     const wrapperClasses = "text-white rounded-[40px] bg-white/8 size-fit \
         backdrop-blur-[100px] shadow-[0_0_2px_0_rgba(0,149,255,75)_inset]"
     const containerClasses = "p-11.5"
-    const descClasses = "text-base text-white/70 py-8.25 inline-block"
+    const descClasses = "text-base text-white/70 py-8.25 inline-block tracking-wide"
     const commonClasses = "w-57.5 h-22.5 bg-white/20 rounded-xl"
 
     return (
