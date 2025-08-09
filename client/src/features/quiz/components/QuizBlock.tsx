419fab0f3e646aa57ac30389eabc9e2453717c5b
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-08 18:26:13 -0700

    Changed size settings

--- client/src/features/quiz/components/QuizBlock.tsx
+++ client/src/features/quiz/components/QuizBlock.tsx
@@ -7,10 +7,11 @@ export default function QuizBlock() {
     
     // Classes
     const wrapperClasses = "text-white rounded-[40px] bg-white/8 size-fit \
-        backdrop-blur-[100px] shadow-[0_0_2px_0_rgba(0,149,255,75)_inset]"
-    const containerClasses = "p-11.5"
+        backdrop-blur-[100px] shadow-[0_0_2px_0_rgba(0,149,255,75)_inset] \
+        max-w-146 max-h-130 flex justify-center items-center"
+    const containerClasses = "p-11.5 size-full"
     const descClasses = "text-base text-white/70 py-8.25 inline-block tracking-wide"
-    const commonClasses = "w-57.5 h-25 bg-white/20 rounded-xl"
+    const commonClasses = "w-full h-full bg-white/20 rounded-xl"
 
     return (
         <section className={wrapperClasses}>
