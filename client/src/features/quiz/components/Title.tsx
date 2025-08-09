338b26a3465ce59901985c46383feca4b32bc251
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-08 18:32:27 -0700

    Added styling for a return button when hovering and implemented useNavigate to return a user back to dashboard

--- client/src/features/quiz/components/Title.tsx
+++ client/src/features/quiz/components/Title.tsx
@@ -1,6 +1,10 @@
 import arrowIcon from "../assets/arrow.png"
+import { useNavigate } from "react-router-dom"
 
 export default function Title() {
+
+    // Hooks
+    const navigate = useNavigate()
 
     // Attributes
     const arrowIconAlt = "GoBack"
@@ -9,12 +13,13 @@ export default function Title() {
     const titleClasses = "flex gap-11.5 items-center"
     const titleTextClasses = "font-lato font-regular tracking-wide text-2xl"
     const btnBackClasses = "bg-white/20 rounded-full size-10 cursor-pointer \
-        flex justify-center items-center"
+        flex justify-center items-center hover:scale-105 hover:bg-white/30 \
+        active:scale-102 active:bg-white/20 transition-transform"
     const iconBackClasses = "size-4.5"
 
     return (
         <div className={titleClasses}>
-            <button className={btnBackClasses}>
+            <button className={btnBackClasses} onClick={() => navigate("/dashboard")}>
                 <img className={iconBackClasses} src={arrowIcon} alt={arrowIconAlt} />
             </button>
             <h1 className={titleTextClasses}>Quiz Settings</h1>
