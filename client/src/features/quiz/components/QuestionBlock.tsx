33963bd6d2a8d1d98764cbef5a458d1dc1562112
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-09 15:25:44 -0700

    Added a couple of more hard-coded questions

--- client/src/features/quiz/components/QuestionBlock.tsx
+++ client/src/features/quiz/components/QuestionBlock.tsx
@@ -6,6 +6,18 @@ export default function QuestionBlock() {
             answer2: {answer: "Yesterday", correct: false},
             answer3: {answer: "Tomorrow", correct: true},
             answer4: {answer: "1900 BC", correct: false},
+        }, 
+        {question: "When was it born?", 
+            answer1: {answer: "Today", correct: true},
+            answer2: {answer: "Yesterday", correct: false},
+            answer3: {answer: "Tomorrow", correct: true},
+            answer4: {answer: "1800 BC", correct: false},
+        }, 
+        {question: "When was it returned?", 
+            answer1: {answer: "Today", correct: false},
+            answer2: {answer: "Yesterday", correct: false},
+            answer3: {answer: "Tomorrow", correct: false},
+            answer4: {answer: "200030 BC", correct: true},
         }
     ]
 
