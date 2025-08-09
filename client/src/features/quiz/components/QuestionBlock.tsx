9da0ff47daf1afafcb61e833a18134cd90eae491
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-08 18:53:47 -0700

    Hard-coded questions for the exam

--- client/src/features/quiz/components/QuestionBlock.tsx
+++ client/src/features/quiz/components/QuestionBlock.tsx
@@ -1,4 +1,14 @@
 export default function QuestionBlock() {
+
+    const questions = [
+        {question: "When was it abandoned?", 
+            answer1: {answer: "Today", correct: false},
+            answer2: {answer: "Yesterday", correct: false},
+            answer3: {answer: "Tomorrow", correct: true},
+            answer4: {answer: "1900 BC", correct: false},
+        }
+    ]
+
     return (
         <h1>Started!</h1>
     )
