dc5b0a42b46537b596c709785d3def8ba6d5feb4
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-09 18:52:21 -0700

    Added more styles to the selection divs

--- client/src/features/quiz/components/Question.tsx
+++ client/src/features/quiz/components/Question.tsx
@@ -19,9 +19,11 @@ export default function Question({ question, index }: QuestionsProps) {
         pb-4 after:content-[''] after:absolute after:w-5/6 after:h-px \
         after:bg-white/80 after:left-0 after:bottom-0 \
         after:shadow-[0_0_12px_rgba(255,255,255,0.75)]"
-    const answerClasses = "text-xl text-white/50"
+    const answersClasses = "ml-3.5 my-14.25 flex flex-col gap-5.75"
+    const answerClasses = "relative text-xl text-white/50 \
+        pb-2.75 answerLine cursor-pointer"
     const ansRadioClasses = ""
-    const ansTextClasses = ""
+    const ansTextClasses = "pl-5"
     const chosenClasses = "text-white font-bold"
     const btnsClasses = ""
     const submitClasses = "cursor-pointer"
@@ -35,22 +37,24 @@ export default function Question({ question, index }: QuestionsProps) {
             <div className={containerClasses}>
                 <h3 className={questionSubtitleClasses}>{question.question}</h3>
 
-                {question.answers.map((ans, key) => 
-                    <div key={key} className={answerClasses}>
-                        <input 
-                            className={ansRadioClasses} 
-                            type={selectionType} 
-                            id={`ans-${index}-${key}`}
-                            onClick={() => setChoice(key)}
-                            checked={choice === key}  
-                        />
-                        <label 
-                            className={`${ansTextClasses} ${choice === key && chosenClasses}`} 
-                            htmlFor={`ans-${index}-${key}`}>
-                                {ans.answer}
-                        </label>
-                    </div>                
-                )}
+                <div className={answersClasses}>
+                    {question.answers.map((ans, key) => 
+                        <div key={key} className={answerClasses}>
+                            <input 
+                                className={ansRadioClasses} 
+                                type={selectionType} 
+                                id={`ans-${index}-${key}`}
+                                onClick={() => setChoice(key)}
+                                checked={choice === key}  
+                            />
+                            <label 
+                                className={`${ansTextClasses} ${choice === key && chosenClasses}`} 
+                                htmlFor={`ans-${index}-${key}`}>
+                                    {ans.answer}
+                            </label>
+                        </div>                
+                    )}
+                </div>
 
                 <div className={btnsClasses}>
                     <input className={submitClasses} type={submitType} value={submitVal} />
