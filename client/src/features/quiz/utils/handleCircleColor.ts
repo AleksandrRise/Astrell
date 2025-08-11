95ba32ce2f6b3b85ce1499b2a4a43f2d64f4898c
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-10 20:18:30 -0700

    Added proper shadow colors to circles

--- client/src/features/quiz/utils/handleCircleColor.ts
+++ client/src/features/quiz/utils/handleCircleColor.ts
@@ -6,27 +6,38 @@ export function handleCircleColor(
 ) {
     const baseClasses = "rounded-full size-5 cursor-pointer transition duration-500"
 
+    const whiteShadow = "shadow-[0_0_5px_2px_rgba(255,255,255,0.5)]"
+    const yellowShadow = "shadow-[0_0_5px_2px_rgba(255,204,0,0.5)]"
+    const greenShadow = "shadow-[0_0_5px_2px_rgba(42,157,143,0.5)]"
+    const redShadow = "shadow-[0_0_5px_2px_rgba(255,53,51,0.5)]"
+
     if (isActive) {
-        const activeClasses = "scale-200 shadow-[0_0_5px_2px_rgba(255,255,255,0.5)]"
+        const activeClasses = "scale-200"
 
         if (ansChoice === null) { 
-            return `bg-white ${activeClasses} ${baseClasses}`
+            return `bg-white ${whiteShadow} ${activeClasses} ${baseClasses}`
         } else if (correctArr[index] !== null) {
-            return `${correctArr[index] ? "bg-success" : "bg-error"} ${activeClasses} ${baseClasses}`
+            return `${correctArr[index] 
+                ? `bg-success ${greenShadow}` 
+                : `bg-error ${redShadow}`} 
+                ${activeClasses} ${baseClasses}`
         } else {
-            return `bg-[#FFCC00] ${activeClasses} ${baseClasses}`
+            return `bg-[#FFCC00] ${yellowShadow} ${activeClasses} ${baseClasses}`
         }
     } else {
         const nonActiveClasses = "scale-100 shadow-[0_0_10px_2px_rgba(255,255,255,0.2)]"
 
         if (ansChoice === null) {
-            return `bg-white/80 ${nonActiveClasses} ${baseClasses}`
+            return `bg-white/80 ${whiteShadow} ${nonActiveClasses} ${baseClasses}`
         } 
         else if (correctArr[index] !== null) {
-            return `${correctArr[index] ? "bg-success" : "bg-error"} ${nonActiveClasses} ${baseClasses}`
+            return `${correctArr[index] 
+                ? `bg-success ${greenShadow}` 
+                : `bg-error ${redShadow}`} 
+                ${nonActiveClasses} ${baseClasses}`
         }
         else {
-            return `bg-[#FFCC00]/80 ${nonActiveClasses} ${baseClasses}`
+            return `bg-[#FFCC00]/80 ${yellowShadow} ${nonActiveClasses} ${baseClasses}`
         }
     }
 }
