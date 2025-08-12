eb456dc15f5751c3e11cf5a22b6a3d3cf94e5f8d
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-11 18:57:31 -0700

    Added a few more comments

--- client/src/pages/QuizPage.tsx
+++ client/src/pages/QuizPage.tsx
@@ -5,8 +5,11 @@ import QuizWrapper from "../features/quiz/components/QuizWrapper"
 
 export default function QuizPage() {
 
+    // States
     const [ hasStarted, setHasStarted ] = useState<boolean>(false)
     const [ totalTime, setTotalTime ] = useState<number>(0)
+
+    // Classes
     const mainClasses = "hero-bg w-full h-screen"
     const containerClasses = `${hasStarted && "bg-blur"} transition-filter size-full \
         flex justify-center items-center overflow-hidden`
