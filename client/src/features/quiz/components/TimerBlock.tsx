60f6a7f200fc59b63cbabcd796dde64857e27703
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-11 19:29:45 -0700

    Added timer border color change according to what time is left

--- client/src/features/quiz/components/TimerBlock.tsx
+++ client/src/features/quiz/components/TimerBlock.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Timer from "./Timer"
 
 type TimerBlock = {
@@ -9,22 +9,22 @@ export default function TimerBlock({ duration }: TimerBlock) {
 
     // States
     const [ time, setTime ] = useState<number>(duration)
+    const [ borderColor, setBorderColor ] = useState<string>("border-white/20")
 
     // Color Handling (300000 = 5 minutes; 60000 = 1 minute)
-    const borderColor = (): string => {
-        if (time <= 300000) {
-            return "border-warning/20"
+    useEffect(() => {
+        if (time <= 300000 && time > 60000) {
+            setBorderColor("border-warning/50")
         } else if (time <= 60000) {
-            return "border-error/20"
-        } else {
-            return "border-white/20"
+            setBorderColor("border-error/50")
         }
-    }
+    }, [time])
+
 
     // Classes
     const wrapperClasses = `w-82 min-h-19 bg-black border-1 ${borderColor} rounded-3xl \
         mt-6.5 animate-[fadeIn_1s_1s_forwards] opacity-0 flex justify-center items-center \
-        text-2xl transition`
+        text-2xl`
 
     return (
         <section className={wrapperClasses}>
