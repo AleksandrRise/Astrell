1d4f406b1af9f1b20b05451ed2a2415406532b84
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-13 15:34:59 -0700

    Added a block for small screen sizes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css'
 import App from './App.tsx'
 import ErrorMessageProvider from './shared/components/ErrorMessageProvider.tsx'
+import ScreenMessage from './shared/components/ScreenMessage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorMessageProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ScreenMessage>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ScreenMessage>
     </ErrorMessageProvider>
   </StrictMode>,
 )
