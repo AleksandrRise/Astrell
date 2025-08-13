5a9454167c9ec615ceca0cc973d0b277604bb7bd
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-13 16:15:46 -0700

    made a logo more responsive

--- client/src/features/dashboard/components/LogoSection.tsx
+++ client/src/features/dashboard/components/LogoSection.tsx
@@ -2,9 +2,9 @@ import Logo from "../../../shared/components/Logo"
 
 export default function LogoSection() {
     // Classes
-    const wrapperClasses = "flex flex-wrap items-center xl:text-3xl cursor-default pt-5 pb-2 \
-        lg:text-2xl"
-    const h1Classes = "pt-1 tracking-wider font-bold font-lato"
+    const wrapperClasses = "flex flex-wrap items-center text-3xl cursor-default pt-5 pb-2 \
+    lg:justify-center xl:justify-start"
+    const h1Classes = "pt-1 tracking-wider font-bold font-lato lg:hidden xl:inline"
 
     return (
         <section className={wrapperClasses}>
