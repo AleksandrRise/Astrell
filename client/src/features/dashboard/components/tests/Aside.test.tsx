17e5eb06dbded83db2f22e77cac8365b326551cf
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-14 17:12:02 -0700

    Wrote a test for Aside

---
+++ client/src/features/dashboard/components/tests/Aside.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react"
+import Aside from "../Aside"
+import { describe, it, expect, vi } from "vitest"
+import userEvent from "@testing-library/user-event" 
+import "@testing-library/jest-dom/vitest"
+
+vi.mock("react-router-dom", async (orig) => {
+    const actual: any = await orig()
+    return {
+        ...actual,
+        useNavigate: () => vi.fn(),
+    }
+})
+
+describe("Aside", () => {
+
+    it("renders without crashing", () => {
+        const { getByText } = render(<Aside />)
+        expect(getByText(/Dashboard/i)).toBeInTheDocument()
+    })
+
+    it("updates input", async () => {
+        const user = userEvent.setup()
+        const input = screen.getByRole('textbox')
+
+        await user.type(input, "dashboard")
+        expect(input).toHaveValue("dashboard")
+
+        await userEvent.clear(input)
+
+        await user.type(input, "boa")
+        expect(input).toHaveValue('boa')
+    })
+
+})
