b259ce09cd9bc66bfe2e919d18ffb622f60632fa
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-18 20:39:32 -0700

    Added env variable

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    strictPort: true,
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: "http://localhost:5000",
-        changeOrigin: true
+export default ({ mode }: any) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  const API_URL = `${process.env.APP_BACKEND_BASEURL ?? 'http://localhost:3000'}`;
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: true,
+      strictPort: true,
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: API_URL,
+          changeOrigin: true
+        },
       },
-    },
-  }
-})
+    }
+  })
+}
