19259d997b3792faf38be7158128107efe855b63
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-23 20:13:01 -0700

    Updated Error Message settings

--- client/src/features/home/components/TextLoading.tsx
+++ client/src/features/home/components/TextLoading.tsx
@@ -1,7 +1,7 @@
 export default function TextLoading() {
 
     // Classes
-    const divClasses = "text-white/70 text-center tracking-wider -mt-15"
+    const divClasses = "text-white/70 text-center tracking-wider md:-mt-20 -mt-15"
     const pClasses = "md:text-2xl text-lg fadeIn"
 
     return (
--- client/src/shared/components/ErrorMessage.tsx
+++ client/src/shared/components/ErrorMessage.tsx
@@ -5,12 +5,14 @@ type ErrorProps = {
 export default function ErrorMessage({ message }: ErrorProps) {
 
     // Classes
-    const wrapperClasses = `h-50 w-100 bg-white absolute top-1/12 left-0 right-0 z-1 rounded-[50px] \
-        flex flex-col justify-center items-center bg-white/8 rounded-4xl text-white\
+    const wrapperClasses = `lg:h-50 md:h-30 h-25 md:w-100 w-50 bg-white \ 
+        absolute md:top-1/12 top-1/9 left-0 right-0 z-2 md:rounded-[50px] \
+        rounded-[30px] flex flex-col justify-center \ 
+        items-center bg-white/8 rounded-4xl text-white \
         shadow-[0_0_2px_0_rgba(0,149,255,0.75)_inset] backdrop-blur-[100px] \
         mx-auto fadeIn`
-    const h1Classes = "font-bold text-red-500 tracking-wide text-2xl"
-    const pClasses = "font-xl font-regular"
+    const h1Classes = "font-bold text-error tracking-wide md:text-2xl text-xl"
+    const pClasses = "md:text-lg text-sm font-regular"
 
     return (
         <div className={wrapperClasses}>
