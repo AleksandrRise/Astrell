60fbafdbd32f7f4185e0020fdf75d2ccc2e381b0
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-23 18:08:25 -0700

    Made a text change in accordance with screen resolution inside a main page

--- client/src/features/home/components/TextOriginal.tsx
+++ client/src/features/home/components/TextOriginal.tsx
@@ -2,6 +2,17 @@ import useWindowDimensions from "../../../shared/utils/useWindowDimensions"
 
 export default function TextOriginal() {
 
+    const determineText = () => {
+        if (useWindowDimensions().width <= 768) {
+            return <p className={pClasses}>Click on me to choose a file.</p>
+        } else {
+            return <p className={pClasses}>
+                    Paste your lecture here, so we<br />
+                    could work on that together.
+                </p>
+        }
+    }
+
     // Classes
     const divClasses = "text-white text-center tracking-wider -mt-20"
     const hClasses = "text-white/80 lg:text-base text-md fadeIn"
@@ -12,10 +23,7 @@ export default function TextOriginal() {
     return (
         <div className={divClasses}>
             <h3 className={hClasses}>Hi there! I am <span className={spanClasses}>Astrell.</span></h3>
-            <p className={pClasses}>
-                Paste your lecture here, so we<br />
-                could work on that together.
-            </p>
+            {determineText()}
             <p className={subTextClasses}>Currently, limited to 1-2 minutes.</p>
         </div>
     )
