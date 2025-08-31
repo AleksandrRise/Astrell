4b08487a835d8c741f3cf350f11f4bbaf0be9830
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-08-30 18:05:36 -0700

    Initialized a test for file inputs

---
+++ client/src/features/home/components/tests/FileInput.test.tsx
@@ -0,0 +1,23 @@
+import { describe, expect, it } from "vitest"
+
+describe("File drop", () => {
+    
+
+    it("Small file", () => {
+
+    })
+
+    it("Huge file", () => {
+        
+    })
+})
+
+describe("File click", () => {
+    it("Small file", () => {
+
+    })
+
+    it("Huge file", () => {
+        
+    })    
+})
