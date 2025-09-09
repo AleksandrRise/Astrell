0acd32b167c242415a74550700bc925cf53bdaae
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-09-08 17:05:12 -0700

    refactor(main) removed .tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css'
 import App from './App.tsx'
-import ErrorMessageProvider from './shared/components/ErrorMessageProvider.tsx'
+import ErrorMessageProvider from './shared/components/ErrorMessageProvider'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
