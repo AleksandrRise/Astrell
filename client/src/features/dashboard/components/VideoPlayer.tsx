9bc55a46436b578f9454fdc025137b8415c41d9b
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-06-18 19:07:05 -0700

    Fixed the issue when a video would be of a different size in a dashboard and would push the block below down

--- client/src/features/dashboard/components/VideoPlayer.tsx
+++ client/src/features/dashboard/components/VideoPlayer.tsx
@@ -1,13 +1,19 @@
-import { APP_BACKEND_BASE } from "../../../shared/utils/APP_BACKEND_BASE"
+import { APP_BACKEND_BASE } from "../../../shared/utils/APP_BACKEND_BASE";
 
 const VideoPlayer = () => {
 
     // Classes
-    const videoClasses = "rounded-3xl row-start-1 min-w-full"
+    const videoClasses =
+        "rounded-3xl row-start-1 w-full max-w-3xl max-h-[500px] object-cover";
 
     return (
-        <video className={videoClasses} src={`${APP_BACKEND_BASE}/api/v1/getVideo`} controls />
-    )
-}
+        <video
+            className={videoClasses}
+            src={`${APP_BACKEND_BASE}/api/v1/getVideo`}
+            controls
+            preload="metadata"
+        />
+    );
+};
 
-export default VideoPlayer
+export default VideoPlayer;
