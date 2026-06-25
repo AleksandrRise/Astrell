9e552c5ff7ac8d8e1165a5c5137b5da578e8821e
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-06-24 18:19:26 -0700

    Updated AiBall improving code readability

--- client/src/features/home/components/AiBall.tsx
+++ client/src/features/home/components/AiBall.tsx
@@ -1,16 +1,18 @@
+import type { ReactNode, Dispatch, SetStateAction } from "react";
+
 import imgSrc from "/assets/aithinkingball.png"
 import FileInput from "./FileInput";
 import Text from "./Text"
 
 type AiBallType = {
-    children: React.ReactNode; 
-    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+    children: ReactNode; 
+    setIsLoading: Dispatch<SetStateAction<boolean>>;
 }
 
 export default function AiBall({ children, setIsLoading }: AiBallType) {
 
     // Classes
-    const imgAlt = "AI Thinking Ball"
+    const imgAlt = "AI Ball"
     const figClasses = "zoomIn"
     const imgClasses = "aiball-animation"
     const sectionClasses = "m-auto"
@@ -20,7 +22,15 @@ export default function AiBall({ children, setIsLoading }: AiBallType) {
             <FileInput setIsLoading={setIsLoading} />
 
             <figure className={figClasses}>
-                <img className={imgClasses} width="450px" height="450px" src={imgSrc} alt={imgAlt} />
+                <img
+                    className={imgClasses}
+                    width={450}
+                    height={450}
+                    src={imgSrc}
+                    alt={imgAlt}
+                    aria-hidden="true"
+                    decoding="async"
+                />
             </figure>
 
             {children}
