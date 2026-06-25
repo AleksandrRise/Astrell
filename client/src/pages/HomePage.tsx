02d915ca6e8c7d1ca3308bac158b12ef291f6bbb
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-06-24 19:13:22 -0700

    Updated HomePage to satisfy new component namings

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,31 +1,30 @@
-import { useState, useContext } from "react"
-import Navbar from "../shared/components/Navbar"
-import AiBall from "../features/home/components/UploadHero.tsx"
-import Info from "../shared/components/Info"
-import ErrorMessage from "../shared/components/ErrorMessage"
-import { ErrorMessageContext } from "../shared/utils/ErrorMessageContext.tsx"
+import { useContext, useState } from "react";
+
+import Navbar from "../shared/components/Navbar";
+import Info from "../shared/components/Info";
+import ErrorMessage from "../shared/components/ErrorMessage";
+import UploadHero from "../features/home/components/UploadHero";
+import UploadStatusText from "../features/home/components/UploadStatusText";
+import { ErrorMessageContext } from "../shared/utils/ErrorMessageContext";
 
 export default function HomePage() {
-
-    // States
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-
-    const [errorText] = useContext(ErrorMessageContext)
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorText] = useContext(ErrorMessageContext);
 
     return (
         <main className="w-full h-screen hero-bg flex flex-col pb-30 relative">
             <Navbar>
                 <Navbar.MenuBtn />
                 <Navbar.Info>
-                    <Info.HintBlock />
+                <Info.HintBlock />
                 </Navbar.Info>
             </Navbar>
 
-            <AiBall setIsLoading={setIsLoading}>
-                <AiBall.Text isLoading={isLoading} />
-            </AiBall>
+            <UploadHero setIsLoading={setIsLoading}>
+                <UploadStatusText isLoading={isLoading} />
+            </UploadHero>
 
             {errorText ? <ErrorMessage message={errorText} /> : null}
         </main>
-    )
+    );
 }
