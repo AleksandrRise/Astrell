80f04fcb673400d3cecace93e337908ec1448870
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-06-24 19:10:40 -0700

    Updated UploadHint component with a different tag and convenient classes distribution

--- client/src/features/home/components/UploadHint.tsx
+++ client/src/features/home/components/UploadHint.tsx
@@ -1,21 +1,20 @@
-export default function HintBlock() {
+export default function UploadHint() {
 
     // Classes
-    const wrapperClasses = "absolute shadow-[0_0_50px_0_#003B6E] w-125 top-20 right-19 \
-    hint-bg hint-rounded text-white hint-animation"
-    const innerWrapperClasses = "px-8 py-13.75 shadow-[0_0_0_1px_#007CE8] hint-rounded"
-    const spanClasses = "text-base text-white/50"
-    const pClasses = "text-xl pt-3"
+    const asideClasses = "absolute shadow-[0_0_50px_0_#003B6E] w-125 top-20 right-19 hint-bg hint-rounded text-white hint-animation";
+    const divClasses = "px-8 py-13.75 shadow-[0_0_0_1px_#007CE8] hint-rounded";
 
     return (
-        <div className={wrapperClasses}>
-            <div className={innerWrapperClasses}>
-                <span className={spanClasses}>How to Start Working With the AI?</span>
-                <p className={pClasses}>
-                    Drag a video file downloaded on your 
-                    computer to the center of the page.
+        <aside className={asideClasses}>
+            <div className={divClasses}>
+                <span className="text-base text-white/50">
+                    How to start working with the AI?
+                </span>
+
+                <p className="text-xl pt-3">
+                    Drag a video file from your computer to the center of the page.
                 </p>
             </div>
-        </div>
-    )
+        </aside>
+    );
 }
