64fd0ec7fa6dbf5dfa257a9e01ac93745b3963cc
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-06-25 18:41:04 -0700

    Updated SidebarProfileButton names

--- client/src/features/dashboard/components/SidebarProfileButton.tsx
+++ client/src/features/dashboard/components/SidebarProfileButton.tsx
@@ -1,21 +1,22 @@
-import profileIcon from "/assets/ProfileIcon.png"
+import profileIcon from "/assets/ProfileIcon.png";
 
-export default function Profile() {
+export default function SidebarProfileButton() {
 
     // Classes
-    const btnClasses = "mt-10 w-full flex items-center gap-3 \
-        shadow-[0_1px_0_0_#333533_inset] rounded-3xl bg-linear-to-r \
-        from-white/5 via-[#999999]/5 to-white/10 px-5 py-3.5 mb-11.25 \
-        lg:justify-center xl:justify-start"
-    const imgClasses = "w-11.25 h-11.25"
-    const spanClasses = "text-base text-white/80 lg:hidden xl:inline"
-
-    const altIcon = "Guest Icon"
+    const buttonClasses = ["mt-10", "w-full", "flex", "items-center", "gap-3", 
+        "shadow-[0_1px_0_0_#333533_inset]", "rounded-3xl", "bg-linear-to-r", 
+        "from-white/5", "via-[#999999]/5", "to-white/10", "px-5", "py-3.5", 
+        "mb-11.25", "lg:justify-center", "xl:justify-start"].join(" ");
 
     return (
-        <button className={btnClasses}>
-            <img className={imgClasses} src={profileIcon} alt={altIcon} />
-            <span className={spanClasses}>Guest</span>
+        <button
+            type="button"
+            className={buttonClasses}
+        >
+            <img className="w-11.25 h-11.25" src={profileIcon} alt="Guest Icon" />
+            <span className="text-base text-white/80 lg:hidden xl:inline">
+                Guest
+            </span>
         </button>
-    )
+    );
 }
