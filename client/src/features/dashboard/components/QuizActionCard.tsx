cd820f23b57029b4f8b63c0161d1bf4c03d87b2c
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-06-25 18:03:15 -0700

    Updated QuizActionCard

--- client/src/features/dashboard/components/QuizActionCard.tsx
+++ client/src/features/dashboard/components/QuizActionCard.tsx
@@ -1,33 +1,41 @@
-import QuizIcon from "/assets/QuizIcon.png"
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
+import quizIcon from "/assets/QuizIcon.png";
 
-export default function Action1() {
+export default function QuizActionCard() {
+    const navigate = useNavigate();
 
-    // Hooks
-    const navigate = useNavigate()
-    
+    function handleClick() {
+        navigate("/quiz");
+    }
 
     // Classes
-    const btnClasses = "rounded-3xl logoGradient-bg p-[2px] flex flex-col text-start \
-        justify-center items-center cursor-pointer hover:scale-102 transition"
-    const quizContainerClasses = ""
-    const quizTextClasses = "xl:text-5xl lg:text-4xl font-bold italic bg-clip-text text-transparent \
-        bg-linear-to-l from-[#999999] to-white tracking-wider"
-    const quizSubtextClasses = "xl:text-lg lg:text-md font-semibold italic"
-    const quizHolderClasses = "flex items-center gap-1"
-    const quizIconClasses = "size-7 -mt-1"
-    const quizIconAlt = "QuizIcon"
+    const buttonClasses = ["rounded-3xl", "logoGradient-bg", "p-[2px]",
+        "flex", "flex-col", "text-start", "justify-center", "items-center",
+        "cursor-pointer", "hover:scale-102", "transition"].join(" ");
+    const spanClasses1 = ["xl:text-lg", "lg:text-md", "font-semibold", "italic"].join(" ");
+    const spanClasses2 = ["xl:text-5xl", "lg:text-4xl", "font-bold", "italic", 
+        "bg-clip-text", "text-transparent", "bg-linear-to-l", "from-[#999999]", 
+        "to-white", "tracking-wider"].join(" ");
 
     return (
-        <button className={btnClasses} onClick={() => navigate('/quiz')}>
-            <div className={quizContainerClasses}>
-                <span className={quizSubtextClasses}>Make a</span>
-                <div className={quizHolderClasses}>
-                    <img className={quizIconClasses} src={QuizIcon} alt={quizIconAlt} />
-                    <span className={quizTextClasses}>Quiz</span>
+        <button
+            type="button"
+            className={buttonClasses}
+            onClick={handleClick}
+        >
+            <div>
+                <span className={spanClasses1}>
+                    Make a
+                </span>
+
+                <div className="flex items-center gap-1">
+                    <img className="size-7 -mt-1" src={quizIcon} alt="Quiz Icon" />
+                    <span className={spanClasses2}>
+                        Quiz
+                    </span>
                 </div>
             </div>
         </button>
-    )
+    );
 }
