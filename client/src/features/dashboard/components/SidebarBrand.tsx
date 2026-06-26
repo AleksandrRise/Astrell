4802fc93be26310498f7d4edbfb416e7e30c3857
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-06-25 18:38:54 -0700

    Updated SidebarBrand renaming everything

--- client/src/features/dashboard/components/SidebarBrand.tsx
+++ client/src/features/dashboard/components/SidebarBrand.tsx
@@ -1,15 +1,19 @@
-import Logo from "../../../shared/components/Logo"
+import Logo from "../../../shared/components/Logo";
 
-export default function LogoSection() {
+export default function SidebarBrand() {
+
     // Classes
-    const wrapperClasses = "flex flex-wrap items-center text-3xl cursor-default pt-5 pb-2 \
-    lg:justify-center xl:justify-start"
-    const h1Classes = "pt-1 tracking-wider font-bold font-lato lg:hidden xl:inline"
+    const sectionClasses = ["flex", "flex-wrap", "items-center", "text-3xl",
+        "cursor-default", "pt-5", "pb-2", "lg:justify-center","xl:justify-start"].join(" ");
+    const h1Classes = ["tracking-wider", "font-bold", "font-lato", "lg:hidden",
+        "xl:inline"].join(" ");
 
     return (
-        <section className={wrapperClasses}>
+        <section className={sectionClasses}>
             <Logo />
-            <h1 className={h1Classes}>ASTRELL</h1>
-        </section>       
-    )
+            <h1 className={h1Classes}>
+                ASTRELL
+            </h1>
+        </section>
+    );
 }
