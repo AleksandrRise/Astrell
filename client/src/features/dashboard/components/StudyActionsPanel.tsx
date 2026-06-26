616e08cb8566fc2719f28bd26068a2e99257fd3c
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-06-25 17:56:31 -0700

    Updated StudyActionsPanel with new renamed components

--- client/src/features/dashboard/components/StudyActionsPanel.tsx
+++ client/src/features/dashboard/components/StudyActionsPanel.tsx
@@ -1,18 +1,18 @@
-import Action1 from "./QuizActionCard"
-import Action2 from "./FlashcardsActionCard"
-import Action3 from "./AskAiActionCard"
+import QuizActionCard from "./QuizActionCard";
+import FlashcardsActionCard from "./FlashcardsActionCard";
+import AskAiActionCard from "./AskAiActionCard";
 
-export default function Actions() {
+export default function StudyActionsPanel() {
 
     // Classes
-    const wrapperClasses = "size-full grid grid-cols-2 grid-rows-2 gap-5.25 \
-        row-start-2 font-lato"
+    const sectionClasses = ["size-full", "grid", "grid-cols-2", 
+        "grid-rows-2", "gap-5.25", "row-start-2", "font-lato"].join(" ")
 
     return (
-        <section className={wrapperClasses}>
-            <Action1 />
-            <Action2 />
-            <Action3 />
+        <section className={sectionClasses}>
+            <QuizActionCard />
+            <FlashcardsActionCard />
+            <AskAiActionCard />
         </section>
-    )
+    );
 }
