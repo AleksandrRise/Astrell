e8031591362fd12ee45fc479122fb36a3748ea36
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-06-25 18:29:30 -0700

    Updated SidebarNavItem storing long classes into constants

--- client/src/features/dashboard/components/SidebarNavItem.tsx
+++ client/src/features/dashboard/components/SidebarNavItem.tsx
@@ -18,21 +18,24 @@ export default function SidebarNavItem({
         }
     }
 
+    // Classes
+    const isActiveClasses = isActive
+        ? "text-white/90 shadow-[0_0_0_1px_#333533] bg-linear-to-r from-[#232323]/50 to-white/15 to-60%"
+        : "text-white/50 hover:shadow-[0_0_0_1px_#333533]/50 hover:bg-linear-to-r hover:from-[#232323]/35 hover:to-white/10 hover:to-60%";
+    const buttonClasses = [
+        "flex", "items-center", "gap-3.75", "px-6", "py-2.5", "w-full", "rounded-lg", "lg:justify-center", "xl:justify-start",
+        isActive 
+            ? "shadow-[0_1px_0_0_#333533_inset]" 
+            : "hover:shadow-[0_1px_0_0_#333533_inset]/50 cursor-pointer"
+    ].join(" ");
+
     return (
         <li
-            className={`w-full rounded-lg ${
-                isActive
-                    ? "text-white/90 shadow-[0_0_0_1px_#333533] bg-linear-to-r from-[#232323]/50 to-white/15 to-60%"
-                    : "text-white/50 hover:shadow-[0_0_0_1px_#333533]/50 hover:bg-linear-to-r hover:from-[#232323]/35 hover:to-white/10 hover:to-60%"
-            }`}
+            className={isActiveClasses}
         >
             <button
                 type="button"
-                className={`flex items-center gap-3.75 px-6 py-2.5 w-full rounded-lg lg:justify-center xl:justify-start ${
-                    isActive
-                        ? "shadow-[0_1px_0_0_#333533_inset]"
-                        : "hover:shadow-[0_1px_0_0_#333533_inset]/50 cursor-pointer"
-                }`}
+                className={buttonClasses}
                 onClick={handleClick}
                 aria-current={isActive ? "page" : undefined}
             >
